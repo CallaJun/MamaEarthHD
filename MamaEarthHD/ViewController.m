4c0c516a1c164bb9c8421ffb9de7005faab03095
Calla <maracondo@aol.com>
2014-10-27 17:29:10 -0400

    Sunny fix

--- MamaEarthHD/ViewController.m
+++ MamaEarthHD/ViewController.m
@@ -42,7 +42,7 @@ BOOL weatherCalled = 0;
         self.currentDesc.text = weather.currentDescription;
         
         //WEATHER CASES
-        if([weather.currentDescription rangeOfString:@"Sunny" options:NSCaseInsensitiveSearch].location != NSNotFound) {
+        if(60 < weather.Fvalue && ([weather.currentDescription rangeOfString:@"Sunny" options:NSCaseInsensitiveSearch].location != NSNotFound)) {
             UIImage *Sunny1 = [UIImage imageNamed:@"Sunny1.png"];
             [_mamaSays setImage:Sunny1];
         }
